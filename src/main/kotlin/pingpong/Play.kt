6c388aeb9a8ee50dcf759788b41190c2196064f9
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-07-14 23:35:07 +0200

    draft

---
+++ src/main/kotlin/pingpong/Play.kt
@@ -0,0 +1,64 @@
+package pingpong
+
+import actor.AbstractActor
+import actor.Actor
+import java.util.concurrent.Semaphore
+
+private val semaphore = Semaphore(1)
+fun main(args: Array<String>) {
+    val referee = object : AbstractActor<Int>("Referee") {
+        override fun onReceive(message: Int, sender: Result<Actor<Int>>) {
+            println("Game ended after $message shots")
+            semaphore.release()
+        }
+    }
+    val player1 =
+        player("Player1", "Ping", referee)
+    val player2 = player("Player2", "Pong", referee)
+    semaphore.acquire()
+    player1.tell(1, Result.success(player2))
+    semaphore.acquire()
+// main thread terminates
+}
+
+private class Player(
+    id: String,
+    private val sound: String,
+    private val referee: Actor<Int>
+) :
+    AbstractActor<Int>(id) {
+    override fun onReceive(message: Int, sender: Result<Actor<Int>>) {
+        println("$sound - $message")
+        if (message >= 10) {
+            referee.tell(message, sender)
+        } else {
+            sender.fold(
+                { actor: Actor<Int> ->
+                    actor.tell(message + 1, self())
+                },
+                { referee.tell(message, sender) }
+            )
+        }
+    }
+}
+
+fun player(
+    id: String,
+    sound: String,
+    referee: Actor<Int>
+) =
+    object : AbstractActor<Int>("id") {
+        override fun onReceive(message: Int, sender: Result<Actor<Int>>) {
+            println("$sound - $message")
+            if (message >= 10) {
+                referee.tell(message, sender)
+            } else {
+                sender.fold(
+                    { actor: Actor<Int> ->
+                        actor.tell(message + 1, self())
+                    },
+                    { referee.tell(message, sender) }
+                )
+            }
+        }
+    }
